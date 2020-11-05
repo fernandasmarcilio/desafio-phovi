e93b4e438a2ae84cff9017384b6a656233f5e1d7
fernanda <fernandas.marcilio@hotmail.com>
2020-11-05 10:51:42 -0300

    Atualiza e conecta firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,18 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebaseApp from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-//Firebase data here
+  apiKey: "AIzaSyATHClbP6vGOJ8SI5jblde07GXnJG_ahFQ",
+  authDomain: "desafio-trivia.firebaseapp.com",
+  databaseURL: "https://desafio-trivia.firebaseio.com",
+  projectId: "desafio-trivia",
+  storageBucket: "desafio-trivia.appspot.com",
+  messagingSenderId: "454824541320",
+  appId: "1:454824541320:web:2b422b369f0a3602b0f33d"
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+
+firebaseApp.initializeApp(firebaseConfig);
+const firebase = firebaseApp.firestore()
 
 export default firebase;
